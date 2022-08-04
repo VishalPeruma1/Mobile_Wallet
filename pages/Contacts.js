5220ec245361901ae1d54a74790f5585865aebd9
Xavier-Alfred <xavier12emmanuel@gmail.com>
2022-08-04 23:29:04 +0530

    Filter options added in contacts page

--- pages/Contacts.js
+++ pages/Contacts.js
@@ -1,28 +1,63 @@
 import React from 'react';
 
 import {View, Text, TouchableOpacity,Image,TextInput,StyleSheet, ScrollView} from 'react-native';
-import { Card, ListItem } from "react-native-elements";
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Card} from "react-native-elements";
 import { useIsFocused } from '@react-navigation/native';
 import Entypo from 'react-native-vector-icons/Entypo';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import Toast from 'react-native-simple-toast';
+import {scale, ScaledSheet} from 'react-native-size-matters';
 
 
 const Contacts = ({navigation}) => {
     
-  const [contact, setContact] = React.useState('')
   const [contacts, setContacts] = React.useState([])
   const [count, setCount] = React.useState()
   const [foundContacts, setFoundContacts] = React.useState([]);
   const [selected, setSelected] = React.useState(null)
   const isFocused = useIsFocused();
-
+  const [search, setSearch] = React.useState("");
 
   React.useEffect(()=>{
     getContactsList()
     getContactsListStatus()
   }, [isFocused])
+
+  const searchFilterFunction = (text) => {
+    console.log("Inside Search")
+    const keyword = text;
+    setSearch(keyword)
+
+    if (keyword!=="") {
+     console.log("Inside IF")
+     console.log(keyword)
+     const results = contacts.filter(t=>Object.values(t).some(nickname=>typeof nickname ==="string" && nickname.toLowerCase().includes(keyword.toLowerCase())));
+     const results2 = contacts.filter(t => Object.values(t).some(did => typeof did === "string" && did.includes(keyword)));
+     console.log(results)
+     console.log(results2)
+     if (results2.length > 0 && results.length === 0) {
+                setFoundContacts(results2);
+                console.log(results2)
+     }
+      if (results.length > 0 && results2.length === 0) {
+          setFoundContacts(results);
+          console.log(results)
+        }
+      if (results.length > 0 && results2.length > 0) {
+            const results3 = [...new Set(results.concat(results2))]
+            setFoundContacts(results3);
+            console.log(results3)
+        }
+      if(results.length===0 && results2.length===0) {
+        setFoundContacts([])
+      }
+                
+      setSelected(null)
+            }
+    else {
+      setContacts(contacts);
+      setSelected(null)
+    }
+  }
 
 
     
@@ -78,16 +113,13 @@ const Contacts = ({navigation}) => {
   const ContactsCard = ({data})=>{
     return(
       <TouchableOpacity onPress={() => navigation.navigate('Contact Details', {"data":data})}>
-        <Card containerStyle={{marginLeft:10, marginRight:10,borderRadius:10,backgroundColor:"white",padding:5, paddingLeft:12, borderColor:"white",elevation:5}} wrapperStyle={{height:105, width:200}}>
-          <View style={{flexDirection:'column', flexWrap:"wrap", justifyContent:"space-evenly"}}>
+        <Card containerStyle={styles.cardContainer} wrapperStyle={{height:scale(90), width:scale(175)}}>
+          <View style={styles.cardView}>
+            <View style={{paddingEnd:5}}>
             {data.dp===""?
               // <Image source={require('../assets/knuct-logo.png')}/>
-              <View style={{backgroundColor:generateColor({data}), borderRadius:40, width: 80, height:80, alignContent:"center", justifyContent:"center"}}>
-              <Text style={{
-                fontSize: 35,
-                color: "#fff",
-                textAlign: "center",
-                 }}>
+              <View style={[styles.indCard,{backgroundColor:generateColor({data})}]}>
+              <Text style={styles.contactName}>
                 {data.nickname.slice(0,1)}
               </Text>
               </View>
@@ -95,14 +127,14 @@ const Contacts = ({navigation}) => {
               <Image source={{
                 uri:`data:image/jpg;base64,${JSON.parse(data.dp).base64}`
               }}
-               style={{height:80,width:80, borderRadius:40}} 
+               style={{height:scale(70),width:scale(70), borderRadius:scale(40)}} 
               />
               // console.log(JSON.parse(data.dp).base64)
             }  
-
-            <View >
-              <Text style={{fontFamily:'Roboto',color:'#000000DE', fontSize:20}}> {data.nickname}</Text>
-              <Text style={{color: 'rgba(0, 0, 0, 0.6)', fontSize:12, fontFamily:'Roboto' , marginLeft:5, paddingTop:10}}> {data.did}</Text>
+            </View>
+            <View>
+              <Text style={styles.nickName}>{data.nickname}</Text>
+              <Text style={styles.did}>{data.did}</Text>
             </View> 
           </View>
         </Card>
@@ -113,35 +145,45 @@ const Contacts = ({navigation}) => {
 
   return (
     <ScrollView>
-    <Card containerStyle={{width:"auto", height:"auto", borderRadius:10, backgroundColor:"white", borderColor:"white", marginBottom:75 }}>
+    <Card containerStyle={styles.mainCard}>
     <View style={{flexDirection:'row'}}>
 
-      <Text style={{fontWeight:'bold',fontFamily:'Roboto',color:'#000000DE', fontSize:20}}>Contacts</Text>
+      <Text style={styles.contText}>Contacts</Text>
       <TouchableOpacity onPress={() => navigation.navigate('New Contact')} style={{flexDirection:"row"}}>
        
-        <Text style={{fontSize:14,marginLeft:135,color:'#1976D2',marginTop:6}}>NEW CONTACT</Text>
+        <Text style={styles.newContText}>NEW CONTACT</Text>
       
       </TouchableOpacity>
       
      
     </View>
-    
-    {
-      (count === 1)?
-      <Text style={{color: '#000000DE', fontSize:12, fontFamily:'Roboto' , marginLeft:5, paddingTop:10}}> {count} Contact</Text>
-        :
-      <Text style={{color: '#000000DE', fontSize:12, fontFamily:'Roboto' , marginLeft:5, paddingTop:10}}> {count} Contacts</Text>
-    }
-    
+    {search===""?
+      <Text style={styles.searchText}>{count} total contact(s)</Text>:
+      <Text style={styles.searchText}>{Object.keys(foundContacts).length} contact(s) found from {count} total contact(s)</Text>
+    }   
+     <View>
+     <TextInput placeholder='Search Contacts' placeholderTextColor="grey" style={styles.textinput} onChangeText={(search) => searchFilterFunction(search)} />
+    </View>
     <View>
-        {contacts.map((data,id) => (
-            <ContactsCard key={id} data={data} />
-        ))}
+        {
+          (() => {
+            if(search!==""){
+              return foundContacts.map((data,id) => (
+                <ContactsCard key={id} data={data} />
+            ))
+            }
+            else{
+             return contacts.map((data,id) => (
+                <ContactsCard key={id} data={data} />
+            ))
+            }
+          })()
+        }
     </View>
 
     <View>
-      <TouchableOpacity style={{backgroundColor:'#1976D2',height:40,width:40,borderRadius:25 ,marginLeft:280, marginTop:20}} onPress={()=>navigation.navigate('New Contact')}>
-          <Entypo name="plus" style={{color:'white',fontSize:35,alignSelf:'center'}}/>      
+      <TouchableOpacity style={styles.plusTO} onPress={()=>navigation.navigate('New Contact')}>
+          <Entypo name="plus" style={styles.plus}/>      
       </TouchableOpacity>
     </View>
     
@@ -149,85 +191,107 @@ const Contacts = ({navigation}) => {
     </ScrollView>
   );
 };
-const styles = StyleSheet.create({
-    sectionContainer: {
-      marginTop: 32,
-      paddingHorizontal: 24,
-    },
-    sectionTitle: {
-      fontSize: 24,
-      fontWeight: '600',
-    },
-    content:{
-      color:'black',
-      flex:2 ,
-      textAlign:'center',
-      fontSize: 15,
-      marginTop: 25,
-      // justifyContent: 'center',
-      // marginLeft: 37
-    },
-    Button_style:{
-      flex:2 ,
-      textAlign:'center',
-      fontSize: 20,
-      marginTop: 25,
-      // justifyContent: 'center',
-      // marginLeft: 37,
-      alignItems: 'center'
-    },
-    container: {
-      alignItems: 'center',
-   },
-   newwallet:{
-    borderColor:'#1976D2',
-    flexDirection:'row',
-    borderWidth:1,
-    height:65,
-    padding:12, 
-    justifyContent:'center',
-    flex:1,
-    borderRadius:5,
-    marginTop:30,
-    alignItems: 'center',
-    backgroundColor:'#1976D2'
+const styles = ScaledSheet.create({
+  dp:{
+    height:'80@s',
+    width:'80@s', 
+    borderRadius:'40@s'
   },
+  did:{
+  color: 'rgba(0, 0, 0, 0.6)',
+  fontSize:'10@s', 
+  fontFamily:'Roboto',
+  marginLeft:'5@s', 
+  paddingTop:'10@s'
+},
+  nickName:{
+    fontFamily:'Roboto',
+    color:'#000000DE',
+    fontSize:'20@s'
+
+  },
+  plus:{
+    color:'white',
+    fontSize:'35@s',
+    alignSelf:'center'
+  },
+
+  plusTO:{
+    backgroundColor:'#1976D2',
+    height:'40@s',
+    width:'40@s',
+    borderRadius:'25@s' ,
+    marginLeft:'250@s', 
+    marginTop:'20@s'
+  },
+  searchText:{
+    color: '#000000DE',
+    fontSize:'12@s', 
+    fontFamily:'Roboto' ,
+    marginLeft:'5@s', 
+    paddingTop:'10@s'
+  },
+  newContText:{
+    fontSize:'14@s',
+    marginLeft:'100@s',
+    color:'#1976D2',
+    marginTop:'6@s'
+  },
+  contText:{
+    fontWeight:'bold',
+    fontFamily:'Roboto',
+    color:'#000000DE', 
+    fontSize:'20@s'
+  },
+  mainCard:{
+    width:"auto", 
+    height:"auto", 
+    borderRadius:'10@s', 
+    backgroundColor:"white", 
+    borderColor:"white",
+    marginBottom:'75@s' 
+  },
+  contactName:{
+    fontSize: '35@s',
+    color: "#fff",
+    textAlign: "center"
+  },
+  indCard:{
+    borderRadius:'40@s', 
+    width: '70@s', 
+    height:'70@s',
+    alignContent:"center", 
+    justifyContent:"center"
+
+  },
+  cardView:{
+  flexDirection:'column', 
+  flexWrap:"wrap", 
+  justifyContent:"space-evenly"
+},
+
+  cardContainer:{
+    marginLeft:'10@s', 
+    marginRight:'10@s',
+    borderRadius:'10@s',
+    backgroundColor:"white",
+    padding:'5@s', 
+    paddingLeft:'12@s', 
+    borderColor:"white",
+    elevation:'5@s'
+  },        
    textinput:{
     color: "black",
-    height: 40,
-    width: 325,
-    marginTop: 25,
-    marginStart:12,
-    marginEnd:12,
-    padding: 10,
-    borderWidth: 1, 
-    borderRadius: 5,
+    height: '40@s',
+    width: '275@s',
+    marginTop: '25@s',
+    marginStart:'12@s',
+    marginEnd:'12@s',
+    padding: '10@s',
+    borderWidth: '1@s', 
+    borderRadius: '5@s',
   },
-   text: {
-      borderWidth: 1,
-      padding: 25,
-      borderColor: 'black',
-      backgroundColor: 'red'
-   }
-  ,
-    headline: {
-      textAlign: 'center', // <-- the magic
-      fontWeight: 'bold',
-      color:'black',
-      fontSize: 35,
-      marginTop: 250,
-      // justifyContent: 'center',
-      // marginLeft: 37,
-      alignItems: 'center'
-    },
-    sectionDescription: {
-      marginTop: 8,
-      fontSize: 18,
-      fontWeight: '400',
-    },
-    highlight: {
-      fontWeight: '700',
-    },
+   
     
   });
 export default Contacts;
